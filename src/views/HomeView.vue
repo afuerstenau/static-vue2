ad2898cf298b665a429b050dffec1fe51a1a89b7
Alex Fürstenau <gitlab@familie-fuerstenau.de>
2023-01-26 15:57:16 +0100

    axios request

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,32 @@
-<template>
-  <div class="home">Das Token ist {{ token }}</div>
-</template>
+<script setup>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+const kundendaten = ref(null);
+onMounted(() => {
+  axios
+    .get(
+      "http://meintest.green-planet-energy.de/p1?method=bestaetigeVertrag&token=TestAFU123",
+      {
+        crossDomain: true,
+        withCredentials: false,
+      }
+    )
+    .then((response) => {
+      console.log(response);
+      console.log(kundendaten);
+    });
+});
+</script>
 
 <script>
 export default {
   name: "HomeView",
-  props: ["token"],
+  props: ["token", "kundendaten"],
 };
 </script>
+
+<template>
+  <div class="home">
+    Das Token ist {{ token }} Kundendaten: {{ kundendaten }}
+  </div>
+</template>
